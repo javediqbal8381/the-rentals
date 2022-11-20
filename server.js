8ce42e42f80ae46836e7bdf68831e9193cdd2f83
javed iqbal <javediqbal8381@gmail.com>
2022-11-20 05:40:13 -0800

    final commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const dbconnecton = require('./db');
@@ -14,10 +13,12 @@ app.use('/api/bookings', require('./routes/bookingsRoute'));
 app.use('/api/autocare', require('./routes/autocareRoute'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static('client/build'));
+  const path = require('path');
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+
+  app.get('/', (req, res) => {
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
