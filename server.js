4a51b32ebe2b9251c911482b5969d6d020c0572e
javed iqbal <javediqbal8381@gmail.com>
2022-11-20 07:39:57 -0800

    fgg

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const dbconnecton = require('./db');
@@ -12,16 +13,12 @@ app.use('/api/users', require('./routes/usersRoute'));
 app.use('/api/bookings', require('./routes/bookingsRoute'));
 app.use('/api/autocare', require('./routes/autocareRoute'));
 
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
 
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
-  app.get('/', (req, res) => {
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')))
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+})
 app.listen(port, () => {
   console.log(`node js server started at port ${port}`);
 });
